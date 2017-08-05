7e29703be6981d7b154195908af2134315c923a0
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-05 22:48:54 +0300

    better git root function

--- plugins/git.sh
+++ plugins/git.sh
@@ -1,7 +1,13 @@
 function configure_git() {
-	# some aliases for git stuff
-	alias git-root='cd $(git rev-parse --show-cdup)'
 	return 0
+}
+
+function git_root() {
+	cd_arg="$(git rev-parse --show-cdup)"
+	if [ ! -z "$cd_arg" ]
+	then
+		cd "$cd_arg"
+	fi
 }
 
 register_interactive configure_git
