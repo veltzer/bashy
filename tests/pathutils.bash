28ad10d4febc28776b7aca0b5ecea306c044b212
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-05 20:01:10 +0300

    fixed up pathutils

--- tests/pathutils.bash
+++ tests/pathutils.bash
@@ -10,11 +10,18 @@ R="/usr/games"
 pathutils_remove P $R
 if [ "$P" != "/usr/bin:/bin" ]
 then
-	echo "ERROR"
+	echo "ERROR1"
 fi
 
 pathutils_add_head R "/bin"
 if [ "$R" != "/bin:/usr/games" ]
 then
-	echo "ERROR"
+	echo "ERROR2"
+fi
+
+before_path=$PATH
+pathutils_add_head PATH "/opt"
+if [ "/opt:$before_path" != "$PATH" ]
+then
+	echo "ERROR3"
 fi
