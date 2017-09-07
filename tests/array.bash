3c1b9e37abb4ce07ec8fee43843f62c03a182497
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-07 16:41:02 +0300

    array now works

--- tests/array.bash
+++ tests/array.bash
@@ -14,4 +14,19 @@ array_new arr2
 array_set arr2 0 a
 array_set arr2 1 b
 array_set arr2 2 c
-array_print arr2
+d=5
+array_pop arr2 d
+if [ "$d" != "c" ]
+then
+	echo "ERROR"
+fi
+array_pop arr2 d
+if [ "$d" != "b" ]
+then
+	echo "ERROR"
+fi
+array_pop arr2 d
+if [ "$d" != "a" ]
+then
+	echo "ERROR"
+fi
