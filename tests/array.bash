3da303e8b445f691490e59ea1fbd695aadea2a70
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-07 15:52:46 +0300

    added array tools

--- tests/array.bash
+++ tests/array.bash
@@ -4,4 +4,14 @@ source ../core/array.bashinc
 
 array_new my_array
 array_set my_array 2 4
-array_print my_array
+array_length my_array len
+if [ "$len" != 1 ]
+then
+	echo "ERROR"
+fi
+
+array_new arr2
+array_set arr2 0 a
+array_set arr2 1 b
+array_set arr2 2 c
+array_print arr2
