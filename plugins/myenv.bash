2ee4f805508edab571fa9cf0c784933aa4bbff2a
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-18 18:02:39 +0300

    cleaner version of myenv

--- plugins/myenv.bash
+++ plugins/myenv.bash
@@ -32,73 +32,43 @@ function myenv_getconf() {
 	assoc_create myenv_conf
 	export myenv_conf
 
+	# first read the configuration from the home folder
 	export myenv_home_conf_file="$HOME/$myenv_conf_file_name"
 	if [ -r "$myenv_home_conf_file" ]
 	then
 		assoc_config_read myenv_conf "$myenv_home_conf_file"
 	fi
-	assoc_get myenv_conf myenv_read_config_from_git_root "read_config_from_git_root"
-	assoc_get myenv_conf myenv_read_config_from_cwd "read_config_from_cwd"
 
-	# get git data if we are in git
-	if git_is_inside
+	# next step over it with local configuration
+	if [ -r "$myenv_conf_file_name" ]
 	then
-		export myenv_in_git=0
-		export myenv_git_root
-		git_top_level myenv_git_root
-		export myenv_git_repo_name
-		git_repo_name myenv_git_repo_name
-	else
-		export myenv_in_git=1
+		assoc_config_read myenv_conf "$myenv_conf_file_name"
 	fi
 
-	# read config from git root if we are in git
-	if [ "$myenv_read_config_from_git_root" = 0 ]
-	then
-		if [ "$myenv_in_git" = 0 ]
-		then
-			local myenv_conf_file="$myenv_git_root/$myenv_conf_file_name"
-			if [ -r "$myenv_conf_file" ]
-			then
-				assoc_config_read myenv_conf "$myenv_conf_file"
-			fi
-		fi
-	fi
-
-	# read config from the current working directory
-	if [ "$myenv_read_config_from_cwd" = 0 ]
-	then
-		if [ -r "$myenv_conf_file_name" ]
-		then
-			assoc_config_read myenv_conf "$myenv_conf_file_name"
-		fi
-	fi
-
-	# set the virual env name
+	# get all the other parameters from the config
+	export myenv_virtual_env_python_version
+	export myenv_virtual_env_auto_create
+	export myenv_virtual_env_auto_activate
+	export myenv_virtual_env_auto_deactivate
 	export myenv_virtual_env_name
-	assoc_get myenv_conf myenv_virtual_env_name "virtual_env_name"
-
-	if null_is_null "$myenv_virtual_env_name"
-	then
-		myenv_virtual_env_name="$myenv_git_repo_name"
-	fi
-
-	# set the folder to the virtual env
-	export myenv_folder="$HOME/.virtualenvs/$myenv_virtual_env_name"
-
-	# set all the other parameters
-	export myenv_python_version myenv_error_deactivate myenv_git_activate myenv_git_deactivate
-	export myenv_auto_method myenv_auto_create myenv_auto_activate myenv_auto_deactivate
+	export myenv_virtual_env_name_by_folder
 	export myenv_debug
-	assoc_get myenv_conf myenv_python_version "python_version"
-	assoc_get myenv_conf myenv_error_deactivate "error_deactivate"
-	assoc_get myenv_conf myenv_git_activate "git_activate"
-	assoc_get myenv_conf myenv_git_deactivate "git_deactivate"
-	assoc_get myenv_conf myenv_auto_method "auto_method"
-	assoc_get myenv_conf myenv_auto_create "auto_create"
-	assoc_get myenv_conf myenv_auto_activate "auto_activate"
-	assoc_get myenv_conf myenv_auto_deactivate "auto_deactivate"
+	assoc_get myenv_conf myenv_virtual_env_python_version "virtual_env_python_version"
+	assoc_get myenv_conf myenv_virtual_env_auto_create "virtual_env_auto_create"
+	assoc_get myenv_conf myenv_virtual_env_auto_activate "virtual_env_auto_activate"
+	assoc_get myenv_conf myenv_virtual_env_auto_deactivate "virtual_env_auto_deactivate"
+	assoc_get myenv_conf myenv_virtual_env_name "virtual_env_name"
+	assoc_get myenv_conf myenv_virtual_env_name_by_folder "virtual_env_name_by_folder"
 	assoc_get myenv_conf myenv_debug "debug"
+
+	# calculate variables from other variables
+
+	if [ "$myenv_virtual_env_name_by_folder" = "0" ]
+	then
+		myenv_virtual_env_name="${PWD##*/}"
+	fi
+	# set the folder to the virtual env
+	export myenv_virtual_env_folder="$HOME/.virtualenvs/$myenv_virtual_env_name"
 }
 
 # function to issue a message if we are in debug mode
@@ -117,33 +87,33 @@ function myenv_msg() {
 }
 
 function myenv_create() {
-	mkdir -p "$myenv_folder"
-	virtualenv --quiet "--python=/usr/bin/python$myenv_python_version" "$myenv_folder"
-	source "$myenv_folder/bin/activate"
-	pip install --quiet -r "$myenv_git_root/requirements.txt"
-	cat "$myenv_git_root/requirements.txt" | md5sum > "$myenv_folder/$myenv_md5_file_name"
+	mkdir -p "$myenv_virtual_env_folder"
+	virtualenv --quiet "--python=/usr/bin/python$myenv_virtual_env_python_version" "$myenv_virtual_env_folder"
+	source "$myenv_virtual_env_folder/bin/activate"
+	pip install --quiet -r "requirements.txt"
+	cat "requirements.txt" | md5sum > "$myenv_virtual_env_folder/$myenv_md5_file_name"
 }
 
 function myenv_recreate() {
-	if [ ! -d "$myenv_folder" ]
+	if [ ! -d "$myenv_virtual_env_folder" ]
 	then
 		myenv_msg "no virtual env found, setting up virtual env"
 		myenv_create
 		return
 	fi
-	if [ ! -f "$myenv_folder/$myenv_md5_file_name" ]
+	if [ ! -f "$myenv_virtual_env_folder/$myenv_md5_file_name" ]
 	then
 		myenv_msg "md5 file is missing, recreating environment"
-		rm -rf "$myenv_folder"
+		rm -rf "$myenv_virtual_env_folder"
 		myenv_create
 		return
 	fi
-	local a=$(cat "$myenv_git_root/requirements.txt" | md5sum)
-	local b=$(cat "$myenv_folder/$myenv_md5_file_name")
+	local a=$(cat "requirements.txt" | md5sum)
+	local b=$(cat "$myenv_virtual_env_folder/$myenv_md5_file_name")
 	if [ "$a" != "$b" ]
 	then
 		myenv_msg "md5 is out of sync, recreating envrionment"
-		rm -rf "$myenv_folder"
+		rm -rf "$myenv_virtual_env_folder"
 		myenv_create
 		return
 	fi
@@ -182,7 +152,7 @@ function myenv_activate_soft() {
 	if ! [ -n "${VIRTUAL_ENV}" ]
 	then
 		myenv_print_debug "activating virtual env"
-		source "$myenv_folder/bin/activate"
+		source "$myenv_virtual_env_folder/bin/activate"
 	fi
 }
 
@@ -193,71 +163,34 @@ function myenv_activate() {
 		return
 	fi
 	myenv_print_debug "activating virtual env"
-	source "$myenv_folder/bin/activate"
+	source "$myenv_virtual_env_folder/bin/activate"
 }
 
 function myenv_prompt_inner() {
 	myenv_getconf
 
-	found_method=1
-	if [ "$myenv_auto_method" = "git" ]
-	then
-		if ! [ "$myenv_in_git" = 0 ] || ! [ -r "$myenv_git_root/requirements.txt" ]
-		then
-			myenv_print_debug "getconf or requirements.txt error"
-			if [ "$myenv_error_deactivate" = 0 ]
-			then
-				myenv_deactivate_soft
-			fi
-			return
-		fi
-		if git_is_inside
-		then
-			if [ "$myenv_git_activate" != 0 ]
-			then
-				myenv_deactivate_soft
-				return
-			fi
-		else
-			if [ "$myenv_git_deactivate" = 0 ]
-			then
-				myenv_deactivate_soft
-				return
-			fi
-		fi
-		found_method=0
-	fi
-	if [ "$myenv_auto_method" = "myenv" ]
-	then
-		if ! [ -r "$myenv_conf_file_name" ] || ! [ -r "requirements.txt" ]
-		then
-			myenv_deactivate_soft
-			return
-		fi
-		found_method=0
-	fi
-	if [ "$found_method" = "1" ]
+	if ! [ -r "$myenv_conf_file_name" ] || ! [ -r "requirements.txt" ]
 	then
 		myenv_deactivate_soft
 		return
 	fi
 
-	if [ "$myenv_auto_deactivate" = 0 ]
+	if [ "$myenv_virtual_env_auto_deactivate" = 0 ]
 	then
 		# if we are in the wrong virtual env, deactivate
 		if myenv_in_virtual_env
 		then
-			if [ $(readlink -f "$myenv_folder") != "$VIRTUAL_ENV" ]
+			if [ $(readlink -f "$myenv_virtual_env_folder") != "$VIRTUAL_ENV" ]
 			then
 				myenv_deactivate
 			fi
 		fi
 	fi
-	if [ "$myenv_auto_create" = 0 ]
+	if [ "$myenv_virtual_env_auto_create" = 0 ]
 	then
 		myenv_recreate
 	fi
-	if [ "$myenv_auto_activate" = 0 ]
+	if [ "$myenv_virtual_env_auto_activate" = 0 ]
 	then
 		myenv_activate_soft
 	fi
@@ -267,9 +200,9 @@ function myenv_prompt() {
 	myenv_prompt_inner
 	if [ "$VIRTUAL_ENV" ]
 	then
-		export myenv_powerline="$myenv_python_version"
+		export myenv_powerline_virtual_env_python_version="$myenv_virtual_env_python_version"
 	else
-		export -n myenv_powerline=""
+		export -n myenv_powerline_virtual_env_python_version=""
 	fi
 }
 
@@ -278,12 +211,7 @@ function myenv_prompt() {
 # of bash.
 function configure_myenv() {
 	local __user_var=$1
-	if ! pathutils_is_in_path virtualenv
-	then
-		var_set_by_name "$__user_var" 1
-		return
-	fi
-	if ! pathutils_is_in_path md5sum
+	if ! pathutils_is_in_path virtualenv md5sum
 	then
 		var_set_by_name "$__user_var" 1
 		return
