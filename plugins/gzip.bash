9839c12323273af261b9b74a226f5d0ec29c499e
Mark Veltzer <mark.veltzer@gmail.com>
2019-06-17 02:44:03 +0300

    gzip stuff

--- plugins/gzip.bash
+++ plugins/gzip.bash
@@ -2,6 +2,7 @@ function configure_gzip() {
 	local __user_var=$1
 	# this means that gzip will not save timestamp and name of
 	# orignal file in the compressed file
+	# the GZIP environment variable is currently deprecated and will be gone in the future
 	export GZIP="--no-name"
 	var_set_by_name "$__user_var" 0
 }
