1b27591d2c1be90a7c98ab72802b6085c479a775
Mark Veltzer <mark.veltzer@gmail.com>
2019-07-28 18:33:32 +0300

    removed GZIP variable

--- plugins/gzip.bash
+++ plugins/gzip.bash
@@ -3,7 +3,7 @@ function configure_gzip() {
 	# this means that gzip will not save timestamp and name of
 	# orignal file in the compressed file
 	# the GZIP environment variable is currently deprecated and will be gone in the future
-	export GZIP="--no-name"
+	# export GZIP="--no-name"
 	var_set_by_name "$__user_var" 0
 }
 
