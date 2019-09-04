3d539fd12a1dfcfd4075829fb8303092031b4e43
Mark Veltzer <mark.veltzer@gmail.com>
2019-09-04 19:27:40 +0300

    some stuff

--- plugins/pureline.bash
+++ plugins/pureline.bash
@@ -1,8 +1,8 @@
 function configure_pureline() {
 	local __user_var=$1
-	if [ -r $HOME/install/pureline/pureline ]
+	if [ -r "$HOME/install/pureline/pureline" ]
 	then
-		source $HOME/install/pureline/pureline ~/.pureline.conf
+		source "$HOME/install/pureline/pureline" "$HOME/.pureline.conf"
 		var_set_by_name "$__user_var" 0
 	else
 		var_set_by_name "$__user_var" 1
