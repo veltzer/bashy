77217e0306fa96eba1004cc0b4ea600a4d0c2e22
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-23 22:39:32 +0300

    fix tmux handling

--- plugins/tmux.bash
+++ plugins/tmux.bash
@@ -1,13 +1,22 @@
 # This runs tmux at the start of a session.
+#
+# References:
+# - https://davidtranscend.com/blog/check-tmux-session-exists-script/
 
 function configure_tmux() {
 	local __user_var=$1
 	if pathutils_is_in_path tmux
 	then
+		# if not in tmux
 		if [[ -z ${TMUX+x} ]]
 		then
-			#exec tmux attach
-			exec tmux
+			session="workspace"
+			if tmux has-session -t $session 2> /dev/null
+			then
+				exec tmux attach-session -t $session
+			else
+				exec tmux new-session -s $session
+			fi
 		fi
 		var_set_by_name "$__user_var" 0
 	else
