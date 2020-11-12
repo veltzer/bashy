bb645e648c8b9d12bde70c362ea601eb826a1e8a
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-12 14:51:18 +0200

    more stuff

--- plugins/k8s.bash
+++ plugins/k8s.bash
@@ -3,6 +3,7 @@ function configure_k8s() {
 	K8S_HOME="${HOME}/install/k8s"
 	if [ -d "${K8S_HOME}" ]
 	then
+		export K8S_HOME
 		pathutils_add_head PATH "${K8S_HOME}"
 		var_set_by_name "$__user_var" 0
 	fi
