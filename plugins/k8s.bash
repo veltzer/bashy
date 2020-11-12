001e53cd243199237b7f20e05c3455abe8f7cb8f
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-12 14:50:44 +0200

    fixes

--- plugins/k8s.bash
+++ plugins/k8s.bash
@@ -1,11 +1,12 @@
 function configure_k8s() {
 	local __user_var=$1
-	if [ -d "${HOME}/install/k8s" ]
+	K8S_HOME="${HOME}/install/k8s"
+	if [ -d "${K8S_HOME}" ]
 	then
-		pathutils_add_head PATH "${HOME}/install/k8s"
+		pathutils_add_head PATH "${K8S_HOME}"
 		var_set_by_name "$__user_var" 0
 	fi
 	var_set_by_name "$__user_var" 1
 }
 
-register configure_go
+register configure_k8s
