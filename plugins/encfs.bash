1f9de63a3557934905637baf0580bfcec2f3098f
Mark Veltzer <mark.veltzer@gmail.com>
2020-12-15 00:57:09 +0200

    more fixes to encfs

--- plugins/encfs.bash
+++ plugins/encfs.bash
@@ -1,19 +1,21 @@
 function configure_encfs() {
 	local __user_var=$1
-	if "$ENCFS_ENABLED"
+	if pathutils_is_in_path encfs
 	then
-		if [ -d "$ENCFS_FOLDER_CLEAR" ] && [ -d "$ENCFS_FOLDER_ENCRYPTED" ]
+		if "$ENCFS_ENABLED"
 		then
-			# mount encrypted file systems: if the folder is not a mountpoint then mount it via encfs
-			# the -o allow_other is very important as it allows other users (in particular the apache
-			# web server which is running under user www-data) to see this folder
-			# mountpoint checks if the mount is already there and ensures we only mount once
-			if mountpoint -q "$ENCFS_FOLDER_CLEAR"
+			if [ -d "$ENCFS_FOLDER_CLEAR" ] && [ -d "$ENCFS_FOLDER_ENCRYPTED" ]
 			then
+				# mountpoint checks if the mount is already there and ensures we only mount once
+				if mountpoint -q "$ENCFS_FOLDER_CLEAR"
+				then
+					var_set_by_name "$__user_var" 0
+					return
+				fi
 				echo "$ENCFS_PASSWORD" | encfs --stdinpass "$ENCFS_FOLDER_ENCRYPTED" "$ENCFS_FOLDER_CLEAR"
+				var_set_by_name "$__user_var" "$?"
+				return
 			fi
-			var_set_by_name "$__user_var" 0
-			return
 		fi
 	fi
 	var_set_by_name "$__user_var" 1
