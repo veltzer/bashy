6554bdca48f16b6e160a85a3c84c184f3d378fe1
Mark Veltzer <mark.veltzer@gmail.com>
2020-12-26 21:40:21 +0200

    better node integration

--- plugins/node.bash
+++ plugins/node.bash
@@ -1,7 +1,15 @@
 function configure_node() {
 	local -n __var=$1
-	__var=0
-	return
+	NODE_HOME="$HOME/install"
+	NODE_HOME_BIN="$NODE_HOME/bin"
+	if [ -d "$NODE_HOME" ] && [ -d "$NODE_HOME_BIN" ]
+	then
+		pathutils_add_head PATH "$NODE_HOME_BIN"
+		export NODE_HOME
+		__var=0
+		return
+	fi
+	__var=1
 }
 
 register_interactive configure_node
