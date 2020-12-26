e21aa9f64ae55c4e3422fc1d443abbbe54c8d4ca
Mark Veltzer <mark.veltzer@gmail.com>
2020-12-26 19:18:04 +0200

    added node plugin

---
+++ plugins/node.bash
@@ -0,0 +1,12 @@
+function configure_node() {
+	local -n __var=$1
+	__var=0
+	return
+}
+
+register_interactive configure_node
+
+function node_install() {
+	sudo apt-get install npm
+	npm completion > "$(HOME)/.bash_completion.d/npm"
+}
