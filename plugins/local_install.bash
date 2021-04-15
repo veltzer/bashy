d6164991d978bf5f1b9eb78e7ca9fed30c62f1e8
Mark Veltzer <mark.veltzer@gmail.com>
2021-04-15 16:54:05 +0300

    added binaries support

--- plugins/local_install.bash
+++ plugins/local_install.bash
@@ -2,14 +2,24 @@ function configure_local_install() {
 	local -n __var=$1
 	LOCAL_INSTALL_BIN="$HOME/install/bin"
 	LOCAL_INSTALL_LIB="$HOME/install/lib"
+	LOCAL_INSTALL_BINARIES="$HOME/install/binaries"
 	if [ -d "$LOCAL_INSTALL_BIN" ] && [ -d "$LOCAL_INSTALL_LIB" ]
 	then
 		pathutils_add_head PATH "$LOCAL_INSTALL_BIN"
 		pathutils_add_head LD_LIBRARY_PATH "$LOCAL_INSTALL_LIB"
-		__var=0
+	else
+		__var=1
 		return
 	fi
-	__var=1
+	if [ -d "$LOCAL_INSTALL_BINARIES" ]
+	then
+		pathutils_add_head PATH "$LOCAL_INSTALL_BINARIES"
+		__var=0
+	else
+		__var=1
+		return
+	fi
+	__var=0
 }
 
 register configure_local_install
