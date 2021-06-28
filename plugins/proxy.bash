07501c74f876ede2c5d3e17ebdf7820e3a033b2b
Mark Veltzer <mark.veltzer@gmail.com>
2021-06-28 08:40:17 +0300

    more

---
+++ plugins/proxy.bash
@@ -0,0 +1,23 @@
+function configure_proxy() {
+	local -n __var=$1
+	if "$PROXY_ENABLED"
+	then
+		proxy_enable
+		__var=1
+	fi
+	__var=0
+}
+
+function proxy_disable() {
+	export -n http_proxy
+	export -n https_proxy
+	export -n no_proxy
+}
+
+function proxy_enable() {
+	export http_proxy="$PROXY_HTTP"
+	export https_proxy="$PROXY_HTTPS"
+	export no_proxy="$PROXY_NO"
+}
+
+register configure_proxy
