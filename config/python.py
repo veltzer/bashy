b356da96e773fc71b376b8e2c2727bb6ede76a12
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-05 01:38:23 +0300

    build it

--- config/python.py
+++ config/python.py
@@ -1,3 +1,13 @@
+test_requires = []
+run_requires = []
+install_requires = []
+setup_requires = []
+dev_requires = [
+    "pymakehelper",
+    "pydmt",
+]
+
+
 test_os = [ "ubuntu-20.04" ]
 test_python = [ "3.9" ]
 test_container = test_os
