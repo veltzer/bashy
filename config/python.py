08d1f161233df590bc1835dc62d85ba332115aaa
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:09:16 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -1,13 +1,7 @@
-test_requires = []
-run_requires = []
-install_requires = []
-setup_requires = []
 dev_requires = [
     "pymakehelper",
     "pydmt",
 ]
 
-
 test_os = [ "ubuntu-20.04" ]
 test_python = [ "3.9" ]
-test_container = test_os
