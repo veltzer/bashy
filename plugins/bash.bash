44fb2677c57d86ddbf439741c9f6e2be91ada788
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-13 20:15:27 +0300

    history stuff

--- plugins/bash.bash
+++ plugins/bash.bash
@@ -1,7 +1,10 @@
 function configure_bash() {
 	local -n __var=$1
 	local -n __error=$2
+	# history stuff. See https://linuxhint.com/bash_command_history_usage
 	HISTSIZE=10000
+	HISTFILESIZE=50000
+	shopt -s histappend
 	__var=0
 }
 
