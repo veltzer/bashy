e865556b2a8b830f4e637bfedc8df1dce05525a0
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-19 23:58:01 +0300

    fixed tests

--- tests/python.bash
+++ tests/python.bash
@@ -2,6 +2,6 @@ source core/python.bash
 
 function testPythonVersion() {
 	a=
-	python_version_short a "$(which python3.10)"
+	python_version_short a "$(command -v python3.10)"
 	assertEquals "$a" 3.10
 }
