caa8e73a3459911fccd73b6e56e84b1964906c0b
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-06 11:43:33 +0300

    no commit message given

--- plugins/xinput.bash
+++ plugins/xinput.bash
@@ -2,7 +2,6 @@ function _activate_xinput() {
 	local -n __var=$1
 	local -n __error=$2
 	xinput --disable "DELL0A89:00 06CB:CE26 Touchpad"
-	xinput --enable "DELL0A89:00 06CB:CE26 Touchpad"
 	__var=0
 }
 
