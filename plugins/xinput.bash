95637ba1e881a7e9f5fef560578ab393c55f207e
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-10 12:36:14 +0300

    xinput fix

--- plugins/xinput.bash
+++ plugins/xinput.bash
@@ -7,12 +7,18 @@
 function _activate_xinput() {
 	local -n __var=$1
 	local -n __error=$2
-	xinput --disable "DELL0A89:00 06CB:CE26 Touchpad"
+	if xinput --list --name-only | grep -q Touchpad
+	then
+		xinput --disable "DELL0A89:00 06CB:CE26 Touchpad"
+	fi
 	__var=0
 }
 
 function xinput_activate() {
-	xinput --enable "DELL0A89:00 06CB:CE26 Touchpad"
+	if xinput --list --name-only | grep -q Touchpad
+	then
+		xinput --enable "DELL0A89:00 06CB:CE26 Touchpad"
+	fi
 }
 
 register _activate_xinput
