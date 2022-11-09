229a9a53f8913d2d6b7ad7beedf96084c6370661
Mark Veltzer <mark.veltzer@gmail.com>
2022-11-09 10:47:26 +0200

    new plugin for core configuring

---
+++ plugins/config_core.bash
@@ -0,0 +1,13 @@
+# This plugin configures core features because
+# default ubuntu does not come with a sane configuration for
+# instruction
+
+function _activate_core() {
+	local -n __var=$1
+	local -n __error=$2
+	ulimit -c unlimited
+	echo core | sudo tee /proc/sys/kernel/core_pattern 
+	__var=0
+}
+
+register _activate_core
