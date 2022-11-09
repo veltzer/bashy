de2849066615bd6c83ad5d927011ecd863556566
Mark Veltzer <mark.veltzer@gmail.com>
2022-11-09 10:48:43 +0200

    small fix

--- plugins/config_core.bash
+++ plugins/config_core.bash
@@ -6,7 +6,7 @@ function _activate_core() {
 	local -n __var=$1
 	local -n __error=$2
 	ulimit -c unlimited
-	echo core | sudo tee /proc/sys/kernel/core_pattern 
+	echo core | sudo tee /proc/sys/kernel/core_pattern > /dev/null
 	__var=0
 }
 
