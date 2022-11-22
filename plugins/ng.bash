2310cc02ce4052ace5830723de77eb3912b9c072
Mark Veltzer <mark.veltzer@gmail.com>
2022-11-22 11:45:19 +0200

    added ng support

---
+++ plugins/ng.bash
@@ -0,0 +1,11 @@
+# This is integration of angular.js (ng on the command line)
+function _activate_ng() {
+	local -n __var=$1
+	local -n __error=$2
+	if ! checkInPath ng __var __error; then return; fi
+	# shellcheck source=/dev/null
+	source <(ng completion script)
+	__var=0
+}
+
+register_interactive _activate_ng
