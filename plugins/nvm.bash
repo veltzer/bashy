3723e4dd94ca0b3c69a56bda7661c9af7634985d
Mark Veltzer <mark.veltzer@gmail.com>
2022-12-07 07:30:53 +0200

    nvm improved

--- plugins/nvm.bash
+++ plugins/nvm.bash
@@ -3,19 +3,14 @@ function _activate_nvm() {
 	local -n __var=$1
 	local -n __error=$2
 	NVM_DIR="$HOME/.nvm"
-	if [ -r "$NVM_DIR/nvm.sh" ]
-	then
-		_bashy_before_thirdparty
-		source "$NVM_DIR/nvm.sh"
-		_bashy_after_thirdparty
-	fi
-	if [ -r "$NVM_DIR/bash_completion" ]
-	then
-		_bashy_before_thirdparty
-		source "$NVM_DIR/bash_completion"
-		_bashy_after_thirdparty
-	fi
-	export NVM_DIR
+	if ! checkReadableFile "$NVM_DIR/nvm.sh" __var __error; then return; fi
+	if ! checkReadableFile "$NVM_DIR/bash_completion" __var __error; then return; fi
+	_bashy_before_thirdparty
+	# shellcheck source=/dev/null
+	source "$NVM_DIR/nvm.sh"
+	# shellcheck source=/dev/null
+	source "$NVM_DIR/bash_completion"
+	_bashy_after_thirdparty
 	__var=0
 }
 
