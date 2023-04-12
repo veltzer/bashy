c655c8d413a2642d80e3e2897a683ee346f6443c
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-12 19:31:08 +0300

    no commit message given

--- core/git.bash
+++ core/git.bash
@@ -1,14 +1,40 @@
 # This is a set of git bash functions for support for bashy.
 
+export _BASHY_CORE_GIT_DEBUG=1
+
+# function to issue a message if we are in debug mode
+function git_debug() {
+	local msg=$1
+	if [ "${_BASHY_CODE_GIT_DEBUG}" = 0 ]
+	then
+		echo "env: debug: $msg"
+	fi
+}
+
+function git_debug_on() {
+	_BASHY_CODE_GIT_DEBUG=0
+}
+
+function git_debug_off() {
+	_BASHY_CODE_GIT_DEBUG=1
+}
+
 # a function that returns whether or not the current working directory
 # is inside a git tree
+# TODO: I'm calling this from lots of plugins for even cd so if I could
+# cache the return value it would be great.
 function git_is_inside() {
-	if ! git rev-parse 2> /dev/null > /dev/null
+	result=$(git rev-parse --is-inside-work-tree 2> /dev/null)
+	err="$?"
+	git_debug "err is ${err}"
+	if [ "${err}" != 0 ]
 	then
-		return "1"
+		return "${err}"
 	fi
-	result=$(git rev-parse --is-inside-work-tree)
 	[ "$result" = "true" ]
+	err2="$?"
+	git_debug "err2 is ${err2}"
+	return "${err2}"
 }
 
 # returns the top level of a git tree
