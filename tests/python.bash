e63679fc074b75cc5c6012dd58e15448f7b584f1
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-21 16:34:15 +0300

    move from python 3.10 to python 3.11

--- tests/python.bash
+++ tests/python.bash
@@ -2,6 +2,6 @@ source core/python.bash
 
 function testPythonVersion() {
 	a=
-	python_version_short a "$(command -v python3.10)"
-	assertEquals "$a" 3.10
+	python_version_short a "$(command -v python3.11)"
+	assertEquals "$a" 3.11
 }
