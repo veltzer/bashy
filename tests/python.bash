9abbd642978e6e2a1cb2d4835f3f8bca16706309
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-22 07:31:35 +0300

    no commit message given

--- tests/python.bash
+++ tests/python.bash
@@ -2,6 +2,14 @@ source core/python.bash
 
 function testPythonVersion() {
 	a=
-	python_version_short a "$(command -v python3.11)"
-	assertEquals "$a" 3.11
+	python_version_short a "$(command -v python3)"
+	ubuntu_ver=$(lsb_release -r -s)
+	if [ "$ubuntu_ver" = "23.04" ]
+	then
+		assertEquals "$a" 3.11
+	fi
+	if [ "$ubuntu_ver" = "22.10" ]
+	then
+		assertEquals "$a" 3.10
+	fi
 }
