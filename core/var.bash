b3a8a7a5d9d0e1067491f8aa096713a546b4cc65
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-01 17:24:05 +0300

    no commit message given

--- core/var.bash
+++ core/var.bash
@@ -3,11 +3,11 @@
 function var_set_by_name() {
 	local __var_name=$1
 	local value=$2
-	eval "$__var_name='$value'"
+	eval "${__var_name}='${value}'"
 }
 
 # return $?
 function var_is_defined() {
 	local __var_name=$1
-	declare -p "$__var_name" > /dev/null 2> /dev/null
+	declare -p "${__var_name}" > /dev/null 2> /dev/null
 }
