acd49e016ccb6f4ba9bdf94d9ed813006967dafa
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-01 16:56:14 +0300

    lint

--- install_in_home.bash
+++ install_in_home.bash
@@ -1,6 +1,5 @@
 #!/bin/bash -eu
 
-# rm -rf "${HOME}/.bashy"
-# rsync --archive --itemize-changes ./ ~/.bashy --exclude-from=.excludes
 rsync -rvnc --delete ./ ~/.bashy --exclude-from=.excludes
-# rsync --archive ./ ~/.bashy --exclude-from=.excludes
+rm -rf "${HOME}/.bashy"
+rsync --archive ./ ~/.bashy --exclude-from=.excludes
