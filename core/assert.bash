0e4a493281f9d33670aea6731d8de8c1aae1a721
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-01 17:18:03 +0300

    lint

--- core/assert.bash
+++ core/assert.bash
@@ -3,20 +3,20 @@ ASSERT_FAILED_CODE=128
 function assertEquals() {
 	local a=$1
 	local b=$2
-	if [ "$a" != "$b" ]
+	if [ "${a}" != "${b}" ]
 	then
-		echo "assertion failed ! [ $a = $b ]"
-		exit $ASSERT_FAILED_CODE
+		echo "assertion failed ! [ ${a} = ${b} ]"
+		exit "${ASSERT_FAILED_CODE}"
 	fi
 }
 
 function assertNotEqual() {
 	local a=$1
 	local b=$2
-	if [ "$a" = "$b" ]
+	if [ "${a}" = "${b}" ]
 	then
-		echo "assertion failed [ $a = $b ]"
-		exit $ASSERT_FAILED_CODE
+		echo "assertion failed [ ${a} = ${b} ]"
+		exit "${ASSERT_FAILED_CODE}"
 	fi
 }
 
@@ -26,5 +26,5 @@ function assertOK() {
 
 function assertFail() {
 	echo "assertion failed"
-	exit $ASSERT_FAILED_CODE
+	exit "${ASSERT_FAILED_CODE}"
 }
