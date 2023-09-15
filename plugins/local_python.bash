f1c92489054b4534680a2e0e3b8a05babb267d36
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-15 06:26:42 +0300

    no commit message given

--- plugins/local_python.bash
+++ plugins/local_python.bash
@@ -1,21 +1,21 @@
 # This plugin allows you to use your own folder (say "${HOME}/install/python")
 # for storing modules which are not well packaged and distributed yet.
 #
-# If you do packages all of your python modules correctly then you don't
+# If you do package all of your python modules correctly then you don't
 # need this module since you can install your modules regularly either
 # at the system level or at the user level.
 
 function _activate_local_python() {
 	local -n __var=$1
 	local -n __error=$2
-	if ! var_is_defined PYTHON_LOCAL
+	if ! var_is_defined LOCAL_PYTHON
 	then
-		__error="PYTHON_LOCAL is not defined"
+		__error="LOCAL_PYTHON is not defined"
 		__var=1
 		return
 	fi
-	if ! checkDirectoryExists "${PYTHON_LOCAL}" __var __error; then return; fi
-	pathutils_add_head PYTHONPATH "${PYTHON_LOCAL}/bin"
+	if ! checkDirectoryExists "${LOCAL_PYTHON}" __var __error; then return; fi
+	pathutils_add_head PYTHONPATH "${LOCAL_PYTHON}/bin"
 	__var=0
 }
 
---
+++ plugins/venv.bash
@@ -0,0 +1,19 @@
+# this plugin helps you activate a customized virtual env
+
+function _activate_venv() {
+	local -n __var=$1
+	local -n __error=$2
+	if ! var_is_defined VENV
+	then
+		__error="VENV is not defined"
+		__var=1
+		return
+	fi
+	if ! checkDirectoryExists "${VENV}" __var __error; then return; fi
+	# activate the virtual envrionment
+	# shellcheck source=/dev/null
+	source "${VENV}/bin/activate"
+	__var=0
+}
+
+register _activate_venv
