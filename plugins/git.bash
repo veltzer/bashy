2c42bb483091e2bf09e47699a35edd056f316954
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-30 14:00:23 +0300

    no commit message given

--- plugins/git.bash
+++ plugins/git.bash
@@ -1,11 +1,8 @@
 function _activate_git() {
 	local -n __var=$1
 	local -n __error=$2
+	if ! checkInPath "git" __var __error; then return; fi
 	__var=0
-}
-
-function git_clean_hard() {
-	git clean -qffxd
 }
 
 register_interactive _activate_git
