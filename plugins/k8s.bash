45fdbe53530a85c60724a85fd9521a40b8ff0338
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-09 06:13:53 +0300

    no commit message given

--- plugins/k8s.bash
+++ plugins/k8s.bash
@@ -12,10 +12,11 @@ function _activate_k8s() {
 }
 
 function _install_k8s() {
+	set +e
 	# instructions for installing k8s are at
 	# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
 	# version=$(curl --silent --location "https://dl.k8s.io/release/stable.txt")
-	version="1.26.7"
+	version="v1.26.7"
 	folder="${HOME}/install/k8s"
 	executable="${folder}/kubectl"
 	echo "installing version ${version}"
