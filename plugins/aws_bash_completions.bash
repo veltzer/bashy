fd0ae67a10b9f48b7037a1810d4e42b95df93307
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-11 14:36:59 +0300

    no commit message given

--- plugins/aws_bash_completions.bash
+++ plugins/aws_bash_completions.bash
@@ -1,17 +1,13 @@
+# Bash completions for the "aws" command
+# The support for this comes from the awscli package and installations
+# for that package is under the "awscli" plugin.
+
 function _activate_aws_bash_completions() {
 	local -n __var=$1
 	local -n __error=$2
-	# completions for aws (must have 'awscli' package from ubuntu
-	# or python module installed)
 	if ! checkInPath "aws_completer" __var __error; then return; fi
 	complete -C aws_completer aws
 	__var=0
-}
-
-function _install_aws_bash_completions() {
-	#/usr/bin/pip install --user awscli
-	# sudo apt install awscli
-	/usr/bin/pip install --user awscliv2
 }
 
 
