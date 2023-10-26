52ffb8dbc1702c3b16bfc181e46fb51e5177b40c
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-26 04:33:58 +0300

    no commit message given

--- core/array.bash
+++ core/array.bash
@@ -6,42 +6,42 @@ function array_new() {
 }
 
 function array_set() {
-	local -n __array=$1
+	local -n __array_set=$1
 	local pos=$2
 	local value=$3
-	__array[pos]="${value}"
+	__array_set[pos]="${value}"
 }
 
 function array_print() {
 	# shellcheck disable=2178
-	local -n __array=$1
-	echo "length of array is ${#__array[@]}"
-	for index in "${!__array[@]}"
+	local -n __array_print=$1
+	echo "length of array is ${#__array_print[@]}"
+	for index in "${!__array_print[@]}"
 	do
-		echo "${index}/${__array[${index}]}"
+		echo "${index}/${__array_print[${index}]}"
 	done
 }
 
 function array_length() {
 	# shellcheck disable=2178
-	local -n __array=$1
+	local -n __array_length=$1
 	local -n var=$2
-	var=${#__array[@]}
+	var=${#__array_length[@]}
 }
 
 function array_push() {
 	# shellcheck disable=2178
-	local -n __array=$1
+	local -n __array_push=$1
 	local var=$2
-	__array+=("${var}")
+	__array_push+=("${var}")
 }
 
 function array_pop() {
 	# shellcheck disable=2178
-	local -n __array=$1
+	local -n __array_pop=$1
 	local -n var=$2
-	var=${__array[${#__array[@]}-1]}
-	unset "__array[${#__array[@]}-1]"
+	var=${__array_pop[${#__array_pop[@]}-1]}
+	unset "__array_pop[${#__array_pop[@]}-1]"
 }
 
 function array_is_array() {
@@ -53,11 +53,11 @@ function array_is_array() {
 
 function array_find() {
 	# shellcheck disable=2178
-	local -n __array=$1
+	local -n __array_find=$1
 	local value=$2
 	local -n __array_location=$3
 	__array_location=0
-	for item in "${__array[@]}"; do
+	for item in "${__array_find[@]}"; do
 		if [ "${item}" = "${value}" ]
 		then
 			return
@@ -69,9 +69,9 @@ function array_find() {
 
 function array_contains() {
 	# shellcheck disable=2178
-	local -n __array=$1
+	local -n __array_contains=$1
 	local value=$2
-	for item in "${__array[@]}"; do
+	for item in "${__array_contains[@]}"; do
 		[ "${item}" = "${value}" ] && return 0
 	done
 	return 1
@@ -79,10 +79,10 @@ function array_contains() {
 
 function array_remove() {
 	# shellcheck disable=2178
-	local -n __array=$1
+	local -n __array_remove=$1
 	local value=$2
 	array_new new_array
-	for elem in "${__array[@]}"
+	for elem in "${__array_remove[@]}"
 	do
 		if [[ "${elem}" != "${value}" ]]
 		then
