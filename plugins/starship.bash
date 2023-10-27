b36a81a9ff640acdc8e23281102309aa01e16097
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-27 14:53:54 +0300

    new starship plugin

--- plugins/starship.bash
+++ plugins/starship.bash
@@ -9,7 +9,10 @@ function _activate_starship() {
 }
 
 function _install_starship() {
-	:
+	# cargo way
+	#cargo install starship --locked
+	# install.sh way
+	BIN_DIR="${HOME}/install/binaries" sh <(curl -sS https://starship.rs/install.sh) -y
 }
 
 register_interactive _activate_starship
