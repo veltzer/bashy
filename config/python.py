1e12b0e20c2cfaa6683b0d879205aca215c6fb78
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 01:52:00 +0200

    new pydmt version

--- config/python.py
+++ config/python.py
@@ -1,9 +1,12 @@
-config_requires = []
-dev_requires = []
-install_requires = []
-make_requires = [
+from typing import List
+
+
+config_requires: List[str] = []
+dev_requires: List[str] = []
+install_requires: List[str] = []
+make_requires: List[str] = [
     "pymakehelper",
     "pydmt",
 ]
-test_requires = []
+test_requires: List[str] = []
 requires = config_requires + install_requires + make_requires + test_requires
