eb94961dbb5eec95ee319af7117e8e0749bf8f1e
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-12 14:59:46 +0200

    fix python.py

--- config/python.py
+++ config/python.py
@@ -6,9 +6,9 @@ dev_requires: List[str] = [
         "pymultigit",
 ]
 install_requires: List[str] = []
-make_requires: List[str] = [
+build_requires: List[str] = [
     "pymakehelper",
     "pydmt",
 ]
 test_requires: List[str] = []
-requires = config_requires + install_requires + make_requires + test_requires
+requires = config_requires + install_requires + build_requires + test_requires
