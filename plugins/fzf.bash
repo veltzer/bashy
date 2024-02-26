9baea1da96bd31b8f424eb3486dbbf90fad0979e
Mark Veltzer <mark.veltzer@gmail.com>
2024-02-26 11:17:26 +0200

    no commit message given

--- plugins/fzf.bash
+++ plugins/fzf.bash
@@ -1,11 +1,10 @@
 function _activate_fzf() {
 	local -n __var=$1
 	local -n __error=$2
-	# this installs fzf for fuzzy matching
-	# https://github.com/junegunn/fzf
 	# it seems that this collides with bash completion
 	# stuff so this must be after the system_deafult script
 	# which does bash completions.
+	if ! checkInPath "fzf" __var __error; then return; fi
 	FILE="${HOME}/.fzf.bash"
 	if ! checkReadableFile "${FILE}" __var __error; then return; fi
 	# shellcheck source=/dev/null
@@ -14,9 +13,12 @@ function _activate_fzf() {
 }
 
 function _install_fzf() {
-	rm -rf "${HOME}/.bashy_install/fzf" > /dev/null 2> /dev/null
-	git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.bashy_install/fzf" > /dev/null 2> /dev/null
-	"${HOME}/.bashy_install/fzf/install" --no-update-rc --key-bindings --completion > /dev/null 2> /dev/null
+	# this installs fzf for fuzzy matching
+	# https://github.com/junegunn/fzf
+	rm -rf "${HOME}/install/fzf" > /dev/null 2> /dev/null
+	git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/install/fzf" > /dev/null 2> /dev/null
+	"${HOME}/install/fzf/install" --no-update-rc --key-bindings --completion > /dev/null 2> /dev/null
+	# sudo apt install fzf
 }
 
 register_interactive _activate_fzf
