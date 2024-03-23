7d354e2a8846ac1c59e7b29b6e372b009a6f0aab
Mark Veltzer <mark.veltzer@gmail.com>
2024-03-23 19:47:01 +0200

    no commit message given

--- plugins/gh.bash
+++ plugins/gh.bash
@@ -7,8 +7,22 @@ function _activate_gh() {
 	__var=0
 }
 
-function _install_gh() {
+function _install_gh_apt() {
 	sudo apt install gh
+}
+
+function _install_gh() {
+	tar="/tmp/gh.tar.gz"
+	rm -f "${tar}"
+	download_file=$(curl --silent --location https://api.github.com/repos/cli/cli/releases/latest | jq --raw-output '.assets[].browser_download_url | select(endswith("_linux_amd64.tar.gz"))')
+	echo "download_file is ${download_file}"
+	curl --location --silent "${download_file}" --output "${tar}"
+	folder="${HOME}/install/binaries"
+	tar xf "${tar}" -C "${folder}" /bin/gh
+	rm -f "${tar}"
+	# there is not need to set the "x" bit on the file since tar extracts it with the right bits
+	# executable="${folder}/lazygit"
+	# chmod +x "${executable}" 
 }
 
 register_interactive _activate_gh
