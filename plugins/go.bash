ed3a4c771a1c46dc9cbd75b2fe301c1fced00035
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-14 17:47:05 +0300

    no commit message given

--- plugins/go.bash
+++ plugins/go.bash
@@ -1,12 +1,12 @@
 function _activate_go() {
 	local -n __var=$1
 	local -n __error=$2
-	GO_PATH="${HOME}/install/go"
-	local GO_PATHBIN="${GO_PATH}/bin"
-	if ! checkDirectoryExists "${GO_PATH}" __var __error; then return; fi
-	if ! checkDirectoryExists "${GO_PATHBIN}" __var __error; then return; fi
-	_bashy_pathutils_add_head PATH "${GO_PATHBIN}"
-	export GO_PATH
+	GO_HOME="${HOME}/install/go"
+	local GO_BIN="${GO_HOME}/bin"
+	if ! checkDirectoryExists "${GO_HOME}" __var __error; then return; fi
+	if ! checkDirectoryExists "${GO_BIN}" __var __error; then return; fi
+	_bashy_pathutils_add_head PATH "${GO_BIN}"
+	export GO_HOME
 	__var=0
 }
 
---
+++ plugins/spark.bash
@@ -0,0 +1,42 @@
+# This is a plugin for spark
+
+function _activate_spark() {
+	local -n __var=$1
+	local -n __error=$2
+	SPARK_HOME="${HOME}/install/spark"
+	local SPARK_BIN="${SPARK_HOME}/bin"
+	if ! checkDirectoryExists "${SPARK_HOME}" __var __error; then return; fi
+	if ! checkDirectoryExists "${SPARK_BIN}" __var __error; then return; fi
+	_bashy_pathutils_add_head PATH "${SPARK_BIN}"
+	export SPARK_HOME
+	__var=0
+}
+
+function _install_spark() {
+	set +e
+	# instructions for installing spark are at
+	# https://medium.com/@patilmailbox4/install-apache-spark-on-ubuntu-ffa151e12e30
+	version="3.5.1"
+	toplevel="spark-${version}-bin-hadoop3"
+	rm -f "${HOME}/install/spark" || true
+	rm -rf "${HOME}/install/${toplevel}" || true
+	url="https://dlcdn.apache.org/spark/spark-${version}/spark-${version}-bin-hadoop3.tgz"
+	echo "url is [${url}]..."
+	curl --location --silent "${url}" | tar xz -C "${HOME}/install"
+	ln -s "${HOME}/install/${toplevel}" "${HOME}/install/spark"
+	set -e
+}
+
+function _uninstall_spark() {
+	folder="${HOME}/install/spark"
+	executable="${folder}/kubectl"
+	if [ -f "${executable}" ]
+	then
+		echo "removing ${executable}"
+		rm "${executable}"
+	else
+		echo "no kubectl detected"
+	fi
+}
+
+register _activate_spark
