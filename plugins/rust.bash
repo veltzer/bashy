d7a24c08b7ce623deb9e8c8731aa2f36390f967f
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-18 06:48:20 +0300

    no commit message given

--- plugins/rust.bash
+++ plugins/rust.bash
@@ -3,8 +3,12 @@ function _activate_rust() {
 	local -n __error=$2
 	CARGO_HOME="${HOME}/.cargo"
 	CARGO_HOME_BIN="${CARGO_HOME}/bin"
+	CARGO_ENV="${CARGO_HOME}/env"
 	if ! checkDirectoryExists "${CARGO_HOME}" __var __error; then return; fi
 	if ! checkDirectoryExists "${CARGO_HOME_BIN}" __var __error; then return; fi
+	if ! checkReadableFile "${CARGO_ENV}" __var __error; then return; fi
+	# shellcheck source=/dev/null
+	# source "${CARGO_ENV}"
 	_bashy_pathutils_add_head PATH "${CARGO_HOME_BIN}"
 	export CARGO_HOME
 	__var=0
