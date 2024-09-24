c3242fcca493de4161d7854096a2b87f95a85c87
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-24 20:53:57 +0300

    no commit message given

--- plugins/prompt_auto.bash
+++ plugins/prompt_auto.bash
@@ -1,15 +1,38 @@
 # This plugin sources .auto.enter.sh whenever it sees it and
 # .auto.exit.sh when it leaves that folder.
+#
+# TODO
+# - this only looks at the current folder and not at the git root
 
 auto_file_enter=".auto.enter.sh"
 auto_file_exit=".auto.exit.sh"
 
 function prompt_auto() {
-	if [ -f "${auto_file_enter}" ]
+	if ! git_is_inside
 	then
 		if var_is_defined AUTO_ACTIVE
 		then
-			if [ "${PWD}" != "${AUTO_ACTIVE}" ]
+			bashy_log "prompt_auto" "${BASHY_LOG_INFO}" "down"
+			auto_file_exit_full="${AUTO_ACTIVE}/${auto_file_exit}"
+			if [ -f "${auto_file_exit_full}" ]
+			then
+				bashy_log "prompt_auto" "${BASHY_LOG_INFO}" "sourcing [${auto_file_exit_full}]"
+				# shellcheck source=/dev/null
+				source "${auto_file_exit_full}"
+			fi
+			unset AUTO_ACTIVE
+		fi
+		return
+	fi
+
+	git_root=""
+	git_top_level git_root
+	auto_file_enter_full="${git_root}/${auto_file_enter}"
+	if [ -r "${auto_file_enter_full}" ]
+	then
+		if var_is_defined AUTO_ACTIVE
+		then
+			if [ "${git_root}" != "${AUTO_ACTIVE}" ]
 			then
 				# we need to get out of a previous auto
 				auto_file_exit_full="${AUTO_ACTIVE}/${auto_file_exit}"
@@ -25,11 +48,10 @@ function prompt_auto() {
 			fi
 		fi
 		# we need to enter the new environment
-		auto_file_enter_full="${PWD}/${auto_file_enter}"
 		bashy_log "prompt_auto" "${BASHY_LOG_INFO}" "sourcing [${auto_file_enter_full}]"
 		# shellcheck source=/dev/null
 		source "${auto_file_enter_full}"
-		export AUTO_ACTIVE="${PWD}"
+		export AUTO_ACTIVE="${git_root}"
 	else
 		if var_is_defined AUTO_ACTIVE
 		then
