b38337ea60f8bdb3de929abedc51aae6578dc72e
Mark Veltzer <mark.veltzer@gmail.com>
2024-12-26 14:34:59 +0200

    no commit message given

---
+++ plugins/kurtosis.bash
@@ -0,0 +1,19 @@
+# This is integration of akurtosisular.js (kurtosis on the command line)
+function _activate_kurtosis() {
+	local -n __var=$1
+	local -n __error=$2
+	if ! checkInPath "kurtosis" __var __error; then return; fi
+	# shellcheck source=/dev/null
+	if ! source <(kurtosis completion bash)
+	then
+		__var=$?
+		__error="could not source kurtosis completion script"
+	fi
+	__var=0
+}
+
+function _install_kurtosis() {
+	:
+}
+
+register_interactive _activate_kurtosis
