d99e8e6f0ef59f08d3ae6aae465e2d20eb092bd7
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-11 21:27:00 +0200

    no commit message given

--- plugins/rust.bash
+++ plugins/rust.bash
@@ -3,16 +3,18 @@ function _activate_rust() {
 	local -n __error=$2
 	if ! checkInPath "cargo" __var __error; then return; fi
 	if ! checkInPath "rustc" __var __error; then return; fi
-	# CARGO_HOME="${HOME}/.cargo"
-	# CARGO_HOME_BIN="${CARGO_HOME}/bin"
-	# CARGO_ENV="${CARGO_HOME}/env"
-	# if ! checkDirectoryExists "${CARGO_HOME}" __var __error; then return; fi
-	# if ! checkDirectoryExists "${CARGO_HOME_BIN}" __var __error; then return; fi
-	# if ! checkReadableFile "${CARGO_ENV}" __var __error; then return; fi
+	CARGO_HOME="${HOME}/.cargo"
+	CARGO_HOME_BIN="${CARGO_HOME}/bin"
+	CARGO_ENV="${CARGO_HOME}/env"
+	if ! checkDirectoryExists "${CARGO_HOME}" __var __error; then return; fi
+	if ! checkDirectoryExists "${CARGO_HOME_BIN}" __var __error; then return; fi
+	if ! checkReadableFile "${CARGO_ENV}" __var __error; then return; fi
+	# sourcing the cargo env file is just adding ~/.cargo/bin to path at the head.
+	# I'd rather do it with my functions.
 	# shellcheck source=/dev/null
 	# source "${CARGO_ENV}"
-	# _bashy_pathutils_add_head PATH "${CARGO_HOME_BIN}"
-	# export CARGO_HOME
+	_bashy_pathutils_add_head PATH "${CARGO_HOME_BIN}"
+	export CARGO_HOME
 	__var=0
 }
 
