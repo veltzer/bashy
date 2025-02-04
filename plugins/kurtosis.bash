49760c3ecc69f4dd7ae567189436c926907f7d54
Mark Veltzer <mark.veltzer@gmail.com>
2025-02-04 14:37:16 +0200

    no commit message given

--- plugins/kurtosis.bash
+++ plugins/kurtosis.bash
@@ -14,7 +14,16 @@ function _activate_kurtosis() {
 }
 
 function _install_kurtosis() {
-	:
+	# set -e
+	local version="2.1.0"
+	local url="https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases/download/${version}/kurtosis-cli_${version}_linux_amd64.tar.gz"
+	local local="/tmp/kurtosis-cli_${version}_linux_amd64.tar.gz"
+	local install_dir="${HOME}/install/binaries"
+	local kurtosis_path="${install_dir}/kurtosis"
+	curl --silent -L "${url}" -o "${local}"
+	tar zxf "${local}" -C "${install_dir}" kurtosis
+	chmod +x "${kurtosis_path}"
+	# set +e
 }
 
 register_interactive _activate_kurtosis
