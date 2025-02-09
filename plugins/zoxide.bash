7952fc21c668e6ded25200e253f9fbad71d7f5dc
Mark Veltzer <mark.veltzer@gmail.com>
2025-02-09 08:33:07 +0200

    no commit message given

--- plugins/zoxide.bash
+++ plugins/zoxide.bash
@@ -5,7 +5,6 @@ function _activate_zoxide() {
 	local -n __var=$1
 	local -n __error=$2
 	if ! checkInPath "zoxide" __var __error; then return; fi
-	if ! checkInPath "fzf" __var __error; then return; fi
 	eval "$(zoxide init bash)"
 	alias cd="z"
 	__var=0
