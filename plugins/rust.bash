da1d262417f25fb268533bcf9819dfd22bdc44e1
Mark Veltzer <mark.veltzer@gmail.com>
2025-05-18 09:34:40 +0300

    new rust plugin

--- plugins/rust.bash
+++ plugins/rust.bash
@@ -3,7 +3,8 @@ function _activate_rust() {
 	local -n __error=$2
 	if ! checkInPath "cargo" __var __error; then return; fi
 	if ! checkInPath "rustc" __var __error; then return; fi
-	CARGO_HOME="${HOME}/.cargo"
+	# CARGO_HOME="${HOME}/.cargo"
+	CARGO_HOME="${HOME}/install/cargo"
 	CARGO_HOME_BIN="${CARGO_HOME}/bin"
 	CARGO_ENV="${CARGO_HOME}/env"
 	if ! checkDirectoryExists "${CARGO_HOME}" __var __error; then return; fi
@@ -18,12 +19,21 @@ function _activate_rust() {
 	__var=0
 }
 
-function _install_rust_2() {
+function _remove_rust() {
+	export CARGO_HOME="${HOME}/install/cargo"
+	rm -rf "${CARGO_HOME}"
+}
+
+function _install_rust_rustup() {
+	export CARGO_HOME="${HOME}/install/cargo"
+	export RUSTUP_HOME="${HOME}/.rustup"
+	rm -rf "${CARGO_HOME}" "${RUSTUP_HOME}"
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
-function _install_rust() {
-	sudo apt install cargo
+function _install_rust_ubuntu() {
+	# these are the ubuntu package for rust
+	sudo apt install cargo rustc rust-src
 }
 
 register _activate_rust
