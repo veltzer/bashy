0333bb5526f4c2f0f9c3ecc963e5328ba97ec5e4
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 10:08:40 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,11 +1,8 @@
-from typing import List
+""" python dependencies for this project """
 
 
-config_requires: List[str] = []
-install_requires: List[str] = []
-build_requires: List[str] = [
-    "pymakehelper",
+build_requires: list[str] = [
     "pydmt",
+    "pymakehelper",
 ]
-test_requires: List[str] = []
-requires = config_requires + install_requires + build_requires + test_requires
+requires = build_requires
