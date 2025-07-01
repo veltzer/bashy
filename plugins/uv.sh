b8d9afdb179c12d196f626e209803f7a180fe3c9
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 04:36:58 +0300

    no commit message given

---
+++ plugins/uv.sh
@@ -0,0 +1,17 @@
+# This is a plugin for uv
+
+function _activate_uv() {
+	local -n __var=$1
+	local -n __error=$2
+	if ! checkInPath "uv" __var __error; then return; fi
+	# shellcheck source=/dev/null
+	if ! source <(uv --generate-shell-completion bash)
+	then
+		__var=1
+		__error="problem in sourcing uv completion"
+		return
+	fi
+	__var=0
+}
+
+register_interactive _activate_uv
