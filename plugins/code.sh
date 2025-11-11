1c67c034c9cbd60c6848b1bafa20f6511c8d8269
Mark Veltzer <mark.veltzer@gmail.com>
2025-11-11 13:01:00 +0200

    no commit message given

--- plugins/code.sh
+++ plugins/code.sh
@@ -1,22 +1,27 @@
 # this is a plugin to install and uninstall vscode from ubuntu 
 
+MSRING="/etc/apt/keyrings/microsoft.gpg"
+MSKEYID="EB3E94ADBE1229CF"
+MSAPT="/etc/apt/sources.list.d/code.sources"
+
 function _install_code() {
-	sudo tee "/etc/apt/sources.list.d/code.sources" > /dev/null << EOF
+	gpg --keyserver "keyserver.ubuntu.com" --recv-keys "${MSKEYID}"
+	gpg --export --armor "${MSKEYID}" | sudo gpg --dearmor -o "${MSRING}"
+	sudo tee "${MSAPT}" > /dev/null << EOF
 Types: deb
 URIs: https://packages.microsoft.com/repos/code
 Suites: stable
 Components: main
 Architectures: amd64
-Signed-By: /usr/share/keyrings/microsoft.gpg
+Signed-By: ${MSRING}
 EOF
-	sudo wget -O "/etc/apt/keyrings/microsoft.asc" "https://packages.microsoft.com/keys/microsoft.asc" --quiet
 	sudo apt update
 	sudo apt install code
 }
 
 function _uninstall_code() {
-	sudo rm "/etc/apt/sources.list.d/code.sources"
-	sudo rm "/etc/apt/keyrings/microsoft.asc"
+	sudo rm "${MSAPT}"
+	sudo rm "${MSRING}"
 	sudo dpkg --purge code
 	sudo apt update
 }
