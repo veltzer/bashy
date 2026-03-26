5bb3c1434acb57527497a2d8ebcf3b6565ec960b
Mark Veltzer <mark.veltzer@gmail.com>
2026-03-26 19:24:09 +0200

    no commit message given

--- plugins/claude.sh
+++ plugins/claude.sh
@@ -1,8 +1,7 @@
 function _activate_claude() {
 	local -n __var=$1
 	local -n __error=$2
-	if ! pass show "keys/claude.ai" &>/dev/null
-	then
+	if ! pass show "keys/claude.ai" &>/dev/null; then
 		__var=$?
 		__error="no pass(1) for [keys/claude.ai] to activate claude.ai"
 		return
@@ -16,7 +15,13 @@ function _activate_claude() {
 function _install_claude_npm() {
 	before_strict
 	# sudo npm install -g "@anthropic-ai/claude-code"
-	npm install -g "@anthropic-ai/claude-code"
+	npm install -g "@anthropic-ai/claude-code@latest"
+	after_strict
+}
+
+function _install_claude_brew() {
+	before_strict
+	brew upgrade claude-code
 	after_strict
 }
 
@@ -24,6 +29,12 @@ function _uninstall_claude() {
 	before_strict
 	# sudo npm uninstall -g "@anthropic-ai/claude-code"
 	npm uninstall -g "@anthropic-ai/claude-code"
+	after_strict
+}
+
+function _uninstall_claude_brew() {
+	before_strict
+	brew uninstall claude-code
 	after_strict
 }
 
