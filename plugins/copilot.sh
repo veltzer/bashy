406eacd923bec2319bd8e1846b11568e44fc547a
Mark Veltzer <mark.veltzer@gmail.com>
2026-04-02 06:42:03 +0300

    no commit message given

--- plugins/copilot.sh
+++ plugins/copilot.sh
@@ -3,8 +3,10 @@ function _activate_copilot() {
 	local -n __error=$2
   # running via gh(1)
 	# alias copilot="gh copilot"
-  # without permission checking
-  alias copilot="gh copilot --allow-all-tools"
+  # without permission checking via gh(1)
+  #alias copilot="gh copilot --allow-all-tools"
+  # without permission checking via npm
+  alias copilot="copilot --allow-all-tools"
 	__var=0
 }
 
@@ -14,9 +16,21 @@ function _install_copilot_npm() {
 	after_strict
 }
 
-function _install_copilt_gh() {
+function _uninstall_copilot_npm() {
 	before_strict
-  gh extension install github/gh-copilot
+  npm uninstall -g "@github/copilot"
+	after_strict
+}
+
+function _install_copilot_gh() {
+	before_strict
+  gh extension install "github/gh-copilot"
+	after_strict
+}
+
+function _uninstall_copilot_gh() {
+	before_strict
+  gh extension remove copilot
 	after_strict
 }
 
