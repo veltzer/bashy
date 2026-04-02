b9da10647ddda904e45b37aebb2fb8ceb269d336
Mark Veltzer <mark.veltzer@gmail.com>
2026-04-02 06:38:01 +0300

    no commit message given

--- plugins/copilot.sh
+++ plugins/copilot.sh
@@ -1,13 +1,22 @@
 function _activate_copilot() {
 	local -n __var=$1
 	local -n __error=$2
-	alias copilot="gh copilot"
+  # running via gh(1)
+	# alias copilot="gh copilot"
+  # without permission checking
+  alias copilot="gh copilot --allow-all-tools"
 	__var=0
 }
 
-function _install_copilot() {
+function _install_copilot_npm() {
 	before_strict
-	npm install -g "@google/copilot-cli"
+  npm install -g "@github/copilot"
+	after_strict
+}
+
+function _install_copilt_gh() {
+	before_strict
+  gh extension install github/gh-copilot
 	after_strict
 }
 
