eb58af66397f4d5a89ad4ff620a434665c3d41e8
Mark Veltzer <mark.veltzer@gmail.com>
2026-07-21 08:40:41 +0300

    no commit message given

--- plugins/python_venv.sh
+++ plugins/python_venv.sh
@@ -3,8 +3,7 @@
 function _activate_python_venv() {
 	local -n __var=$1
 	local -n __error=$2
-	if ! var_is_defined PYTHON_VENV
-	then
+	if ! var_is_defined PYTHON_VENV; then
 		__error="PYTHON_VENV is not defined"
 		__var=1
 		return
@@ -12,8 +11,7 @@ function _activate_python_venv() {
 	if ! checkDirectoryExists "${PYTHON_VENV}" __var __error; then return; fi
 	# activate the virtual envrionment
 	# shellcheck source=/dev/null
-	if ! source "${PYTHON_VENV}/bin/activate"
-	then
+	if ! source "${PYTHON_VENV}/bin/activate"; then
 		__var=$?
 		__error="could not activate virtual env"
 		return
@@ -21,4 +19,4 @@ function _activate_python_venv() {
 	__var=0
 }
 
-register_interactive _activate_python_venv
+register _activate_python_venv
