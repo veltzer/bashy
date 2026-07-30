b868720b1659f8a95c9db7f319527cfd94eedd58
Mark Veltzer <mark.veltzer@gmail.com>
2026-07-30 11:40:24 +0300

    no commit message given

--- plugins/python.sh
+++ plugins/python.sh
@@ -1,20 +1,11 @@
 # This plugin helps with various python related issues
 #
 # What does it provide?
-# - configuring the python keyring so it would work.
 # - the pyrun command to run python code with good auto-comletion.
 
 function _activate_python() {
 	local -n __var=$1
 	local -n __error=$2
-	# export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
-	# shellcheck source=/dev/null
-	# you need to "pip install 'keyring[completion]'" for this to work
-	if ! bashy_completion python python -m keyring --print-completion bash; then
-		__var=1
-		__error="problem in sourcing keyring completion"
-		return
-	fi
 	__var=0
 }
 
